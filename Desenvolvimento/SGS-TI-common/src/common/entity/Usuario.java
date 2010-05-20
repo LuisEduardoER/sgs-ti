a759de0bfd7d051ec30abb3338e6ce48ee1a7d67
denisfgurski <denisfgurski@113a8705-9bcf-ea06-c1d7-6deb3ed10fb8>
2010-05-20 05:02:39 +0000

    Usuario novo

--- Desenvolvimento/SGS-TI-common/src/common/entity/Usuario.java
+++ Desenvolvimento/SGS-TI-common/src/common/entity/Usuario.java
@@ -5,18 +5,15 @@ import java.io.Serializable;
 public class Usuario implements Serializable
 {
 	private static final long serialVersionUID = 1L;
+	
+	private int codigo;
+	private String nome;
 	private String username;
 	private String password;
 	
-	/**
-	* TODO - Documentar
-	*/
 	public Usuario() {
 	}
 	
-	/**
-	* TODO - Descrever melhor os campos
-	*/
 	/**
 	 * 
 	 * @param username
@@ -27,30 +24,12 @@ public class Usuario implements Serializable
 		this.password = password;
 	}
 	
-	/*
-	 * GETTERs AND SETTERs
-	 */
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	/**
-	* TODO - Documentar
-	*/
 	@Override
-	public int hashCode() 
-	{
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + codigo;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		result = prime * result
 				+ ((password == null) ? 0 : password.hashCode());
 		result = prime * result
@@ -58,12 +37,8 @@ public class Usuario implements Serializable
 		return result;
 	}
 
-	/**
-	* TODO - Documentar
-	*/
 	@Override
-	public boolean equals(Object obj) 
-	{
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -71,6 +46,13 @@ public class Usuario implements Serializable
 		if (getClass() != obj.getClass())
 			return false;
 		Usuario other = (Usuario) obj;
+		if (codigo != other.codigo)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
 		if (password == null) {
 			if (other.password != null)
 				return false;
@@ -83,4 +65,40 @@ public class Usuario implements Serializable
 			return false;
 		return true;
 	}
+
+	/*
+	 * GETTERs AND SETTERs
+	 */
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
