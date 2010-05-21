8517033b7eddad25aba3dc1bdeea19a4053c10c7
denisfgurski <denisfgurski@113a8705-9bcf-ea06-c1d7-6deb3ed10fb8>
2010-05-21 23:16:47 +0000

    EIA

--- Desenvolvimento/SGS-TI-service/src/persistencia/util/Conexao.java
+++ Desenvolvimento/SGS-TI-service/src/persistencia/util/Conexao.java
@@ -21,6 +21,7 @@ public class Conexao {
 	private static final String ARQUIVO_CONF = "META-INF/conexao.properties";
 	private static Connection con;
 	private static final Properties configuracoes;
+	private static Conexao instance;
 
 	/*
 	 * código-fonte estático que é executado assim que o ClassLoader carrega a
@@ -43,51 +44,69 @@ public class Conexao {
 		}
 	}
 	
+	private Conexao() {
+		try {
+			con = criarConexao();
+			System.out.println("conexao criada");
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Conexao getInstance(){
+		if(instance == null)
+			instance = new Conexao();
+		return instance;
+	}
+	
 	/**
 	 * Recupera uma nova conexão
 	 * @return Connection
 	 * @throws BusinessException 
 	 */
 	public static Connection obterConexao() throws BusinessException {
-
-		if(con==null){
-			String origem  = configuracoes.getProperty("jdbc.origem");
-
-			String driver = configuracoes.getProperty(
-					origem+".jdbc.driver");
-			String url = configuracoes.getProperty(
-					origem+".jdbc.url");
-			String usuario = configuracoes.getProperty(
-					origem+".jdbc.usuario");
-			String senha = configuracoes.getProperty(
-					origem+".jdbc.senha");
-
-			try {
-				Class.forName(driver);
-
-				Connection con = DriverManager.getConnection(url,usuario,senha);
-
-				return con;
-
-			} catch (Exception e) {
-				throw new BusinessException(SystemConstant.MSG_AM_ERRO_CONEXAO);
-			}
-		}else
-			return con;
+		if(instance == null)
+			instance = new Conexao();
+		return con;
 	}
 
+	public Connection criarConexao() throws BusinessException {
+
+		String origem  = configuracoes.getProperty("jdbc.origem");
+
+		String driver = configuracoes.getProperty(
+				origem+".jdbc.driver");
+		String url = configuracoes.getProperty(
+				origem+".jdbc.url");
+		String usuario = configuracoes.getProperty(
+				origem+".jdbc.usuario");
+		String senha = configuracoes.getProperty(
+				origem+".jdbc.senha");
+
+		try {
+			Class.forName(driver);
+			System.out.println("abrindo conexao...");
+			Connection con = DriverManager.getConnection(url,usuario,senha);
+			
+			return con;
+
+		} catch (Exception e) {
+			throw new BusinessException(SystemConstant.MSG_AM_ERRO_CONEXAO);
+		}
+}
 	/**
 	 * Fecha uma conexão.
 	 * @param con conexão.
 	 */
 	public static void fecharConexao(Connection con) {
 
-		try {
+		/*try {
+			System.out.println("fechando conexao..");
 			if(con != null && !con.isClosed())
 				con.close();
 		} catch (SQLException e) {
 			throw new RuntimeException("Erro ao fechar conexão",e);
-		}
+		}*/
 		
 	}
 	
